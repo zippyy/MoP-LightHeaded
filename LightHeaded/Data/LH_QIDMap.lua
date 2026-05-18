7a6e38d67963319485f291009404f4d637391808
Zippy <zippyy@zoho.com>
2026-05-18 11:49:19 -0600

    Add Midnight data pack mapping

--- LightHeaded/Data/LH_QIDMap.lua
+++ LightHeaded/Data/LH_QIDMap.lua
@@ -4,12 +4,14 @@ LH_QIDMap = {
   [3] = 12762,
   [4] = 25485,
   [5] = 28749,
+  [6] = 90000,
   ["vars"] = {
     [1] = "LH_Data_A",
     [2] = "LH_Data_B",
     [3] = "LH_Data_C",
     [4] = "LH_Data_D",
     [5] = "LH_Data_E",
+    [6] = "LH_Data_Midnight",
   },
   ["addons"] = {
     [1] = "LightHeaded_Data_A",
@@ -17,5 +19,6 @@ LH_QIDMap = {
     [3] = "LightHeaded_Data_C",
     [4] = "LightHeaded_Data_D",
     [5] = "LightHeaded_Data_E",
+    [6] = "LightHeaded_Data_Midnight",
   },
 }
