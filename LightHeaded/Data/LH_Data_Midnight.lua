3cd1830e4b9e54a3b6b69769aacba3aff2c815bb
Zippy <zippyy@zoho.com>
2026-05-18 11:49:03 -0600

    Add placeholder Midnight quest data table

---
+++ LightHeaded/Data/LH_Data_Midnight.lua
@@ -0,0 +1,14 @@
+LH_Data_Midnight = LH_Data_Midnight or {
+--[[
+Format:
+[questID] = {
+  "qid\031sharable\031level\031reqlevel\031startType\031startName\031startID\031endType\031endName\031endID\031exp\031rep\031series\030",
+  "user\031userRating\031rating\031sticky\031date\031comment",
+}
+]]
+
+[92995] = {
+  "92995\0311\03180\03180\031NPC\031Archivist Lysander\0310\031NPC\031Archivist Lysander\0310\0316100\031\031\030",
+  "zippyy\0310\0315\0310\0312026-05-18\031This is a sample Midnight quest entry generated for the new LightHeaded Midnight runtime. Replace this with real Wowhead/community data using the generator workflow."
+},
+}
